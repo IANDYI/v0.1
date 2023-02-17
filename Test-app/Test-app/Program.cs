bef27d61a6a826b0ebde3931b6db9be1b6f4d21d
IANDYI <113261029+IANDYI@users.noreply.github.com>
2023-02-17 14:05:03 +0100

    Start-method

--- Test-app/Test-app/Program.cs
+++ Test-app/Test-app/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+
         }
     }
 }
